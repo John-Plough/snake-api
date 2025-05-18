b890b4c06121fb77c9429459583942b25d1a0e55
John-Plough <jplough@gmail.com>
2025-05-17 21:10:38 -0400

    Update CORS configuration for GitHub Pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV["CORS_ORIGINS"] || [
-      "http://localhost:5173",           # Development
-      "https://john-plough.github.io"    # Production
+      "http://localhost:5173",                                    # Development
+      "https://john-plough.github.io",                           # Production base domain
+      "https://john-plough.github.io/honeymaker"                 # Production with path
     ]
 
     resource "*",
