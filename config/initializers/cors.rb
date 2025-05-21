6797ab7ca73c14399113ad71938ac45dbead9553
John-Plough <jplough@gmail.com>
2025-05-21 15:27:21 -0400

    added nake.onrender.com to allowed origins in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "localhost:3000", "honeymaker.onrender.com"
+    origins "localhost:5173", "localhost:3000", "honeymaker.onrender.com", "snake.onrender.com"
     resource "*",
              headers: :any,
              methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
